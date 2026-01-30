8d1a7f698702998ed5a715bcfa7945483428bba7
Jinyan Chen <jinyanc@login-eos02.eos.clusters.nvidia.com>
2026-01-29 22:59:49 -0800

    update

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 set -ex
 
 # cd /home/jinyanc/github/srt-slurm
-# srtctl apply -f recipies/h200/8k1k/bs128-agg-tp.yaml
 
 srtctl apply -f recipes/h100/1k1k/mtp/h100-fp8-2p4d-max-dep-mtp.yaml
-# srtctl apply -f recipes/h100/1k1k/stp/h100-fp8-2p4d-max-tp.yaml
-# srtctl apply -f recipes/h100/8k1k/mtp/h100-fp8-2p2d-max-dep-mtp.yaml
+
+srtctl apply -f recipes/h100/1k1k/stp/h100-fp8-2p4d-max-tp.yaml
+
+srtctl apply -f recipes/h100/8k1k/mtp/h100-fp8-2p2d-max-dep-mtp.yaml
